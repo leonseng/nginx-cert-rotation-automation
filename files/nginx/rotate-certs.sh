01461403339a64a37d9969e091c7169172fbe2ac
Leon Seng <w.leon.seng@gmail.com>
2024-07-01 11:41:05 +1000

    Added more comments to rotate-certs.sh

--- files/nginx/rotate-certs.sh
+++ files/nginx/rotate-certs.sh
@@ -2,14 +2,21 @@
 
 set -e
 
-secrets=$(aws secretsmanager list-secrets --filters "Key=tag-key,Values=${secrets_manager_target_tag}" | jq -r '[.SecretList[].Name]|join(" ")')
+# Fetch a list of secrets of interest. Using filter to find secrets with certain tags as AWS Secrets Manager does not support multi-tenancy
+secrets=$(aws secretsmanager list-secrets --filters "Key=tag-key,Values=nginx-cert-rotation" | jq -r '[.SecretList[].Name]|join(" ")')
 echo "Found $secrets"
 
-# Loop through each secret name and echo it
+# Loop through each secret name, fetch the the key pair values and store them locally in /etc/nginx/certs
 for secret_id in $secrets; do
   echo "Processing $secret_id";
+
+  # certificate public key is stored in the cert_b64 key, base64 encoded
   aws secretsmanager get-secret-value --secret-id "$secret_id" | jq -r .SecretString | jq -r .cert_b64 | base64 -d > "/etc/nginx/certs/$secret_id.crt";
+
+# certificate public key is stored in the key_b64 key, base64 encoded
   aws secretsmanager get-secret-value --secret-id "$secret_id" | jq -r .SecretString | jq -r .key_b64 | base64 -d > "/etc/nginx/certs/$secret_id.key";
+
+  echo "Success";
 done
 
 # reload nginx if is running
